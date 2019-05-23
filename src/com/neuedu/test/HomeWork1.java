91b5b08fb0d851ad09cccfa51c1e92af6aa5789b
wuxiaorongpl <1085039612@qq.com>
2019-05-23 20:00:26 +0800

    emm

---
+++ src/com/neuedu/test/HomeWork1.java
@@ -0,0 +1,13 @@
+package com.neuedu.test;
+
+public class HomeWork1 {
+    public static void main(String[] args){
+        int a=123;
+        int b=0;
+        while(a !=0){
+            b=a%10;
+            System.out.println(b);
+            a=a/10;
+        }
+    }
+}
